090a3c78a69d2263941ab072bb3f6faae1d68a3e
Aum Paudel <paudelaum@gmail.com>
2026-06-01 15:50:43 +0530

    simple web server

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI 
+app = FastAPI()
+@app.get("/")
+def home():
+    return {"message": "Hello World"}
+@app.get("/about")
+def about():
+    return {"message": "This is about page"}
